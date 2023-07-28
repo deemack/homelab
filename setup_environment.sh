e1e94ff8c7f6e2b3b9e25a8714d11e8cd18bd4ec
deemack <72137221+deemack@users.noreply.github.com>
2023-07-28 21:39:40 +1000

    Update setup_environment.sh

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 sudo apt update && sudo apt upgrade 
-apt-get install virtualbox -y
+sudo apt-get install virtualbox -y
 wget https://releases.hashicorp.com/vagrant/2.2.19/vagrant_2.2.19_x86_64.deb
 sudo apt install ./vagrant_2.2.19_x86_64.deb
 mkdir ~/homelab
 cd ~/homelab
-vagrant init bento/ubuntu-22.04 
-vagrant up
+sudo vagrant init bento/ubuntu-22.04 
+sudo vagrant up
