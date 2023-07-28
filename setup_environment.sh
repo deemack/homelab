bd433ddedd65d3486549efde52b8e3fe3b5e5155
deemack <72137221+deemack@users.noreply.github.com>
2023-07-28 21:04:22 +1000

    Update setup_environment.sh

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt update && sudoo apt upgrade 
+sudo apt update && sudo apt upgrade 
 apt-get install virtualbox -y
 wget https://releases.hashicorp.com/vagrant/2.2.19/vagrant_2.2.19_x86_64.deb
 sudo apt install ./vagrant_2.2.19_x86_64.deb
