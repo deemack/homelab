13f8f63611b757e9552c1d26d2750cb5a961d0df
deemack <72137221+deemack@users.noreply.github.com>
2023-07-29 10:17:48 +1000

    Update setup_environment.sh

--- setup_environment.sh
+++ setup_environment.sh
@@ -5,7 +5,7 @@ wget https://releases.hashicorp.com/vagrant/2.2.19/vagrant_2.2.19_x86_64.deb
 sudo apt install ./vagrant_2.2.19_x86_64.deb
 git clone https://github.com/deemack/homelab.git
 cd homelab
-vagrant init bento/ubuntu-22.04 
+#vagrant init bento/ubuntu-22.04 
 vagrant up
 printf "%s" "Waiting for virtual machine to be ready"
 while ! ping -c 1 -n -w 1 192.168.56.10 &> /dev/null
