5b017f6afcce8cccd3d59a4297ac919c410cab99
deemack <72137221+deemack@users.noreply.github.com>
2023-09-12 13:38:53 +1000

    Update wikijs_backup.sh

--- ansible/playbooks/roles/microk8s_wikijs/files/wikijs_backup.sh
+++ ansible/playbooks/roles/microk8s_wikijs/files/wikijs_backup.sh
@@ -9,5 +9,5 @@
 pod_str=$(microk8s kubectl get pods -n wikijs -l=app=pgsql --no-headers -o custom-columns=":metadata.name")
 
 # Create backup and append date stamp
-d=$(date +%Y-%m-%d)
+d=$(date +%Y-%m-%d-%H.%M.%S)
 microk8s kubectl exec -it -n wikijs $pod_str -- /bin/bash -c "pg_dump wikijs -U wikijs -F t > /var/lib/postgresql/data/$d-wikibackup.tar"
