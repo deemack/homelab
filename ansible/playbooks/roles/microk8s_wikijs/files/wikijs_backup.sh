e0a8a7ef1a8e80b0f15a62fc5fc7df5d26c54c0e
deemack <72137221+deemack@users.noreply.github.com>
2023-09-12 12:49:37 +1000

    Create wikijs_backup.sh

---
+++ ansible/playbooks/roles/microk8s_wikijs/files/wikijs_backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script will run as a cron job to backup the wikijs database each night.
+# It will create a backup with a time stamp, which will be copied to /mnt/storage/backups/wikijs/ 
+# A separate cron job will remove old backups after a specified time
+
+# Get the name of the wikijs postgres pod
+#microk8s kubectl get pods -n wikijs -l=app=pgsql --no-headers -o custom-columns=":metadata.name"
+pod_str=$(microk8s kubectl get pods -n wikijs -l=app=pgsql --no-headers -o custom-columns=":metadata.name")
+
+# Create backup and append date stamp
+d=$(date +%Y-%m-%d)
+microk8s kubectl exec -it -n wikijs $pod_str -- /bin/bash -c "pg_dump wikijs -U wikijs -F t > /var/lib/postgresql/data/$d-wikibackup.tar"
